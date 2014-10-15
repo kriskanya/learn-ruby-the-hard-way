b58e5ae9e1103de88ca181a3da31300c5e8e98d0
Kristian Kanya <kriskanya@gmail.com>
2014-10-15 11:21:28 -0500

    finished 48

--- projects/ex48/lib/ex48/lexicon.rb
+++ projects/ex48/lib/ex48/lexicon.rb
@@ -1,25 +1,11 @@
 class Lexicon
-  attr_accessor :words, :verbs, :stop_words, :nouns, :number_pattern
-
-# Struct is Ruby method which operates like a hash; see below:
-
-# Customer = Struct.new(:name, :address) do
-#   def greeting
-#     "Hello #{name}!"
-#   end
-# end
-#
-# dave = Customer.new("Dave", "123 Main")
-# dave.name     #=> "Dave"
-# dave.greeting #=> "Hello Dave!"
 
 # %w is a whitespace separated array
-
   @@words = %w[north south east west down up left right back]
   @@verbs = %w[go stop kill eat]
   @@stop_words = %w[the in of from at it]
   @@nouns = %w[door bear princess cabinet]
-  @@number_pattern = "[0-9]+"
+  @@number_pattern = /[0-9]+/
 
   def self.scan(input)
 
@@ -27,8 +13,8 @@ class Lexicon
 
     user_input = input.split
 
-    user_input.each do |word|
-      word = word.downcase
+    user_input.each do |w|
+      word = w.downcase
       # if the words array includes the first inputted word, make it into
       # a hash and then push it into the sentence array
       if @@words.include?(word)
@@ -39,10 +25,10 @@ class Lexicon
         sentence.push(["stop", word])
       elsif @@nouns.include?(word)
         sentence.push(["noun", word])
-      elsif @@number_pattern.match(input)
-        sentence.push(["number", input.to_i])
+      elsif @@number_pattern.match(word)
+        sentence.push(["number", word.to_i])
       else
-        sentence.push(["error", word])
+        sentence.push(["error", w])
       end
     end
     return sentence
